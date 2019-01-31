f4651e4995f02cf8491455f445bc697cc06053b7
wenjietseng <wenjietseng@gmail.com>
2019-01-31 18:58:19 +0800

    0113

---
+++ 0716/contest-scoreboard.cpp
@@ -0,0 +1,10 @@
+// Contest score board uva 10258
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+using namespace std;
+#define MAXN 1000
